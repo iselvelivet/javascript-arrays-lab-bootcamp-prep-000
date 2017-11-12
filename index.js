ad709e67700d20061685a9d8d8ae3fab1bc04062
Tim Sander <tomzander88@yahoo.com>
2017-11-12 19:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
 function Arrays () {
   var kittens = ['Milo', 'Otis', 'Garfield']
 }
+function kittens () {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+}
+function destructivelyAppendKitten (name) {
+  kittens.appends(name)
+}
+
