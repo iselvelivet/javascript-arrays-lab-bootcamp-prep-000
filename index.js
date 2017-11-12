a4cb23446bf02e6ff0674fe7915ab09c4af7608a
Tim Sander <tomzander88@yahoo.com>
2017-11-12 19:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,8 @@ function appendKitten (name) {
   kittens = [...kittens, name]
   return kittens
 }
+function prependKitten (name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+  kittens = [name, ...kittens]
+  return kittens
+} 
